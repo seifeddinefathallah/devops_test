299318cb17420120cd0ce5c34aae66452fd6bbaf
seifeddinefathallah <seifathallah7@gmail.com>
2022-10-29 16:49:46 +0100

    From Seifeddine 2

--- src/test/java/com/esprit/examen/services/CategorieProduitServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/CategorieProduitServiceImplTest.java
@@ -6,12 +6,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import java.util.List;
 import com.esprit.examen.entities.CategorieProduit;
+import com.esprit.examen.repositories.CategorieProduitRepository;
+
 import java.text.ParseException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import org.assertj.core.api.Assertions;
 import org.junit.*;
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,7 +24,8 @@ import lombok.extern.slf4j.Slf4j;
 public class CategorieProduitServiceImplTest {
 	@Autowired
 	ICategorieProduitService CategorieProduitService;
-
+	@Autowired
+   private CategorieProduitRepository CategorieProduitRepo;
 	@Test
 	public void testaddCategorieProduits() {
 		CategorieProduit cp;
@@ -49,9 +53,27 @@ public class CategorieProduitServiceImplTest {
 		}
 	@Test
 	public void testupdateCategorieProduit() {
-		
+
+		CategorieProduit cat = CategorieProduitRepo.findById(1L).get();
+        cat.setCodeCategorie("smart");
+        CategorieProduit updateCategorieProduit =  CategorieProduitRepo.save(cat);
+        Assertions.assertThat(updateCategorieProduit.getCodeCategorie()).isEqualTo("smart");
 	}
+	@Test
+	 public void testretrieveCategorieProduits(){
 
+		CategorieProduit cat = CategorieProduitRepo.findById(1L).get();
+			System.out.println(cat);
+	        Assertions.assertThat(cat.getIdCategorieProduit()).isEqualTo(1L);
 
+	    }
+	@Test
+    public void testgetListOfCategorieProduits(){
+
+        List<CategorieProduit> cat = CategorieProduitRepo.findAll();
+        System.out.println("la liste de CategorieProduit est" +cat);
+        Assertions.assertThat(cat.size()).isGreaterThan(0);
+
+    }
 
 }
