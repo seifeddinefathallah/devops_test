6fb0e916ad8e55d7f03c302299e0f28d5e98cd56
seifeddinefathallah <seifathallah7@gmail.com>
2022-11-01 13:59:14 +0100

    From Seifeddine Fatthallah 42

--- src/main/java/com/esprit/examen/controllers/ProduitRestController.java
+++ src/main/java/com/esprit/examen/controllers/ProduitRestController.java
@@ -37,7 +37,8 @@ public class ProduitRestController {
 	@PostMapping("/add-produit")
 	@ResponseBody
 	public Produit addProduit(@RequestBody Produit p) {
-		return produitService.addProduit(p);
+		Produit produit = produitService.addProduit(p);
+		return produit;
 	}
 
 
