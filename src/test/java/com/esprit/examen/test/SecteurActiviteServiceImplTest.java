2133736043c739ee80fe98c137a4f1ff060c6136
seifeddinefathallah <seifathallah7@gmail.com>
2022-11-09 09:49:48 +0100

    From Seifeddine Fatthallah 109

--- src/test/java/com/esprit/examen/test/SecteurActiviteServiceImplTest.java
+++ src/test/java/com/esprit/examen/test/SecteurActiviteServiceImplTest.java
@@ -2,14 +2,12 @@ package com.esprit.examen.test;
 
 import static org.junit.Assert.assertNotNull;
 
-import java.util.Optional;
-
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 
 import com.esprit.examen.entities.SecteurActivite;
 import com.esprit.examen.repositories.SecteurActiviteRepository;
