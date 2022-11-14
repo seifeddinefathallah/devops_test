0a93d8e39e76c0724dada4073b2a571f961fdb02
mohamed98387 <79638613+mohamed98387@users.noreply.github.com>
2022-11-14 10:28:46 +0100

    git-commit

--- src/test/java/com/esprit/examen/services/StockServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplTest.java
@@ -1,4 +1,4 @@
-/*package com.esprit.examen.services;
+package com.esprit.examen.services;
 
 import static org.junit.Assert.*;
 import java.util.List;
@@ -48,4 +48,4 @@ public class StockServiceImplTest {
 		assertNull(stockService.retrieveStock(savedStock.getIdStock()));
 	}
 
-}*/
+}
