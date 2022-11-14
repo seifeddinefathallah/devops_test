62d46201398df704d8cc8980037c36ff20b28e77
rayen kharbeche <rayen.kharbecheesprit.tn>
2022-11-14 03:12:02 +0100

    docker-compose

--- src/main/java/com/esprit/examen/services/FournisseurServiceImpl.java
+++ src/main/java/com/esprit/examen/services/FournisseurServiceImpl.java
@@ -79,7 +79,6 @@ public class FournisseurServiceImpl implements IFournisseurService {
 		Fournisseur fournisseur = fournisseurRepository.findById(idFournisseur).orElse(null);
 		SecteurActivite secteurActivite = secteurActiviteRepository.findById(idSecteurActivite).orElse(null);
         fournisseur.getSecteurActivites().add(secteurActivite);
-        
         fournisseurRepository.save(fournisseur);
 		
 		
